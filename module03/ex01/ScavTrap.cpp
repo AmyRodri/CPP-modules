f3292e33668c6db97f28f7b4c2324ff433d6b067
amyyrodri <yguuuto@gmail.com>
2026-02-24 22:33:41 -0300

    fix take damage ande repaired

--- module03/ex01/ScavTrap.cpp
+++ module03/ex01/ScavTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: kamys <kamys@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 17:17:48 by kamys             #+#    #+#             */
-/*   Updated: 2026/02/23 17:31:34 by kamys            ###   ########.fr       */
+/*   Updated: 2026/02/24 22:32:13 by kamys            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,10 +47,38 @@ ScavTrap::~ScavTrap()
 
 void	ScavTrap::attack(const std::string &target)
 {
+	if (this->hitPoints <= 0 || this->energyPoints <= 0)
+	{
+		std::cout << "ClapTrap " << this->name << " cannot attack!" << std::endl;
+		return ;
+	}
+	this->energyPoints--;
 	std::cout	<< "ScavTrap " << this->name
 				<< " attacks " << target
 				<< ", causing " << attackDamage
 				<< " points of damage!" << std::endl; 
+}
+
+void	ScavTrap::takeDamage(unsigned int amount)
+{
+	if (this->hitPoints <= 0)
+	{
+		std::cout << "ScavTrap " << this->name << " is dead!" << std::endl;
+		return ;
+	}
+	this->hitPoints -= amount;
+	std::cout << "ScavTrap " << name << " takes " << amount << " points of damage!" << std::endl;
+}
+
+void	ScavTrap::beRepaired(unsigned int amount)
+{
+	if (this->hitPoints <= 0 || this->energyPoints <= 0)
+	{
+		std::cout << "ScavTrap " << this->name << " cannot repair!" << std::endl;
+		return ;
+	}
+	this->energyPoints--;
+	std::cout << "ScavTrap " << name << " repairs itself for " << amount << " hit points!" << std::endl;
 }
 
 void	ScavTrap::guardGate()
