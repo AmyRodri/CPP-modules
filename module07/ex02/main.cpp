5722b9426553ac53c59c1a9dbca972b315a8fe00
Amy Rodrigues <rmmgustavo@gmail.com>
2026-06-03 19:20:44 -0300

    fix: test self assignment

--- module07/ex02/main.cpp
+++ module07/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: AmyRodri <amyrodri@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: amyrodri <amyrodri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/02 14:30:12 by AmyRodri          #+#    #+#             */
-/*   Updated: 2026/06/02 14:30:12 by AmyRodri         ###   ########.fr       */
+/*   Updated: 2026/06/03 19:19:22 by amyrodri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,7 +54,9 @@ int main()
 
 	std::cout << "\n=== TESTE 5: SELF ASSIGNMENT ===\n";
 
-	numbers = numbers;
+	Array<int> *ptr = &numbers;
+	
+	numbers = *ptr;
 
 	for (unsigned int i = 0; i < numbers.size(); i++)
 		std::cout << numbers[i] << " ";
@@ -88,3 +90,58 @@ int main()
 	return 0;
 }
 
+
+// #include <iostream>
+// #include <cstdlib>
+// #include "Array.hpp"
+
+// #define MAX_VAL 750
+// int main(int, char**)
+// {
+//     Array<int> numbers(MAX_VAL);
+//     int* mirror = new int[MAX_VAL];
+//     srand(time(NULL));
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         const int value = rand();
+//         numbers[i] = value;
+//         mirror[i] = value;
+//     }
+//     //SCOPE
+//     {
+//         Array<int> tmp = numbers;
+//         Array<int> test(tmp);
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         if (mirror[i] != numbers[i])
+//         {
+//             std::cerr << "didn't save the same value!!" << std::endl;
+//             return 1;
+//         }
+//     }
+//     try
+//     {
+//         numbers[-2] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+//     try
+//     {
+//         numbers[MAX_VAL] = 0;
+//     }
+//     catch(const std::exception& e)
+//     {
+//         std::cerr << e.what() << '\n';
+//     }
+
+//     for (int i = 0; i < MAX_VAL; i++)
+//     {
+//         numbers[i] = rand();
+//     }
+//     delete [] mirror;//
+//     return 0;
+// }
